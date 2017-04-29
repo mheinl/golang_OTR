b285c316cdf30ff99c869db37fe9d63a0d5ee9b4
cableman127 <trile@letech.com>
2017-04-28 20:34:27 -0400

    Adds HMAC and Digital Signature.

--- otr.go
+++ otr.go
@@ -5,17 +5,18 @@ import (
 	"strings"
 	"strconv"
 	"time"
+	"crypto"
 	"crypto/aes"
 	"crypto/cipher"
 	//"crypto/dsa"
 	"crypto/rsa"
 	"crypto/md5"
 	"crypto/x509"
-	//"crypto/sha256"
+	"crypto/sha256"
 	// alias mrand for "math/rand" and crand for "crypto/rand" to avoid confusion
 	mrand "math/rand"
 	crand "crypto/rand"
-	//"crypto/hmac"
+	"crypto/hmac"
 	"errors"
 	"fmt"
 	"io"
@@ -181,6 +182,7 @@ func generateKeys(bit int) (rsa.PublicKey, *rsa.PrivateKey, error){
 	return testPub, test, nil
 }
 
+// Generates PEM KEY files from RSA Keys
 func generatePEMKeys(privFileName string, pubFileName string, pubkey rsa.PublicKey, privkey *rsa.PrivateKey) ([]byte, []byte, error) {
 	privateKeyPEM, err := savePEMKey(privFileName,privkey)
 	checkError(err)
@@ -189,13 +191,49 @@ func generatePEMKeys(privFileName string, pubFileName string, pubkey rsa.PublicK
 	return privateKeyPEM, publicKeyPEM, nil
 }
 
-
+// Generates 32 character MD5 Hash from DH shared secret
 func generateMD5Hash(sharedSecret int) (string) {
 	byteSecret := []byte(strconv.Itoa(sharedSecret))
 	h := md5.New()
 	h.Write([]byte(byteSecret))
 	base32str := hex.EncodeToString(h.Sum(nil))
 	return base32str
+}
+
+// Checks if HMAC created from ciphertext and key is valid
+func checkMAC(message, messageMac, key []byte) bool {
+	mac := hmac.New(sha256.New, key)
+	mac.Write(message)
+	expectedMAC := mac.Sum(nil)
+	return hmac.Equal(messageMac, expectedMAC)
+}
+
+// Generates HMAC from ciphertext and key
+func generateMAC(message, key []byte) ([]byte) {
+	mac := hmac.New(sha256.New, key)
+	mac.Write(message)
+	hashMAC := mac.Sum(nil)
+	return hashMAC
+}
+
+// Generates Signature from RSA private key and hash
+func generateSignature(rsaPrivateKey *rsa.PrivateKey, hash []byte) ([]byte) {
+	signature, err := rsa.SignPKCS1v15(crand.Reader, rsaPrivateKey, crypto.SHA256, hash[:])
+	if err != nil {
+		fmt.Printf("Error from signing: %s\n", err)
+		return nil
+	}
+	return signature
+}
+
+// Checks if Signature created from hash and RSA private key is valid
+func verifySignature(rsaPublicKey *rsa.PublicKey, hash []byte, signature []byte) bool {
+	err := rsa.VerifyPKCS1v15(rsaPublicKey, crypto.SHA256, hash[:], signature)
+	if err != nil {
+		fmt.Printf("Error verification of signature: %s\n", err)
+		return false
+	}
+	return true
 }
 
 /***************** Main *****************/
@@ -250,6 +288,17 @@ func main() {
 	fmt.Printf("Decrypted Plaintext: %s\n", result)
 	// End of AES Encryption
 	
-	
+
+	// HMAC
+	MACHash := generateMAC(ciphertext, key)
+	success := checkMAC(ciphertext, MACHash, key)
+	fmt.Println(MACHash)
+	fmt.Println(success)
+
+	// Signature
+	signature := generateSignature(alicePrivateKey, MACHash)
+	verified := verifySignature(&alicePublicKey, MACHash, signature)
+	fmt.Println(hex.EncodeToString(signature))
+	fmt.Println(verified)
 }
 
